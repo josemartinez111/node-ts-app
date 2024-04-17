984d799a68d589b5714c5758b4fc5cec5c8a8832
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2024-04-17 04:18:25 -0400

    Updated app.ts

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,50 @@
 // FILE: app/app.ts
 // _______________________________________________
+
+import { fastify, FastifyReply, FastifyRequest } from "fastify";
 // _______________________________________________
+
+/** --------------------------------------------------------- */
+/**                      HTTP-SERVER                          */
+/** --------------------------------------------------------- */
+
+const fast = fastify();
+fast.get("/", async (_request: FastifyRequest, _reply: FastifyReply) => {
+	return {
+		key: "Hola",
+		metedata: {
+			value: "mundo",
+			status: "200-OK",
+		}
+	};
+});
+
+const optsPort = { port: 8080 };
+
+fast.listen(optsPort, () => {
+	console.log(`Server is running on: http://localhost:${ optsPort.port }`);
+});
+/** --------------------------------------------------------- */
+/**                MAIN_FUNCTION_FOR_CONSOLE_APP              */
+/** --------------------------------------------------------- */
 
 const main = () => {
+	spacer("=");
+	// __________________________________________________________
+	
+	
 	console.log("\n");
 	console.log("Hola mundo!");
+	// __________________________________________________________
+	spacer("=");
 };
-// _______________________________________________
 
 main();
-// _______________________________________________
+/** --------------------------------------------------------- */
+/**                      CODE_SANDBOX                         */
+/** --------------------------------------------------------- */
+
+function spacer(char: string = "_", lengthOfChar: number = 55) {
+	return char.repeat(lengthOfChar);
+}
+// __________________________________________________________
